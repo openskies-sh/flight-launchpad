55e9c1828e38f44ab0a912ffeef33366f58dcc26
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2021-03-18 12:52:34 +0000

    Added additional views and new form handling capabiltiy

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,53 @@
 var express = require('express');
 var router = express.Router();
-
+const formidable = require('formidable');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 
-router.post('/upload', (req, res) => {
+router.post('/upload', (req,  res, next) => {
   //req.fields contains non-file fields 
   //req.files contains files 
-  res.send(JSON.stringify(req.fields));
+  // res.send(JSON.stringify(req.fields));
+  const form = formidable({ multiples: true });
+ 
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.render('preview', {
+      title: "Preview Operation",
+      userProfile: userProfile,
+      errors: {},
+      data: {
+        'fields': fields,
+        'geojson': geojson
+      }
+    });
+    
+  });
+});
+
+router.post('/submit-operation', (req,  res, next) => {
+    res.render('status', {
+      title: "Acceptance Status",
+      errors: {},
+      data: {
+      }
+    });
+    
+});
+router.post('/operation-status', (req,  res, next) => {
+    res.render('status', {
+      title: "Operation Status",
+      errors: {},
+      data: {
+      }
+    });
+    
 });
 
 module.exports = router;
