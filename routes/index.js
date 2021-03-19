0a9c3187dd50f8fbffa44da5f01e800550b9e21e
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2021-03-19 11:10:43 +0000

    Added form handling and new file upload capacity

--- routes/index.js
+++ routes/index.js
@@ -1,54 +1,74 @@
 var express = require('express');
 var router = express.Router();
 const formidable = require('formidable');
+
+const gjv = require("geojson-validation");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
 });
 
 
-router.post('/upload', (req,  res, next) => {
+router.post('/upload', (req, res, next) => {
   //req.fields contains non-file fields 
   //req.files contains files 
   // res.send(JSON.stringify(req.fields));
-  const form = formidable({ multiples: true });
- 
+  // const form = formidable({ multiples: true });
+
+  var form = new formidable.IncomingForm();
+
+
+
   form.parse(req, (err, fields, files) => {
     if (err) {
       next(err);
       return;
     }
-    res.render('preview', {
-      title: "Preview Operation",
-      userProfile: userProfile,
-      errors: {},
-      data: {
-        'fields': fields,
-        'geojson': geojson
-      }
-    });
+    console.log(fields);
+    console.log(files);
     
+  
+  res.render('preview', {
+    title: "Preview Operation",
+    errors: {},
+    data: {
+      'fields': fields,
+      // 'geojson': geojson
+    }
   });
+
+  });
+
+  return;
+
+  
+  // res.render('preview', {
+  //   title: "Preview Operation",
+  //   errors: {},
+  //   data: {
+  //     // 'fields': fields,
+  //     // 'geojson': geojson
+  //   }
+  // });
 });
 
-router.post('/submit-operation', (req,  res, next) => {
-    res.render('status', {
-      title: "Acceptance Status",
-      errors: {},
-      data: {
-      }
-    });
-    
+router.get('/submit-operation', (req, res, next) => {
+  res.render('status', {
+    title: "Acceptance Status",
+    errors: {},
+    data: {}
+  });
+
 });
-router.post('/operation-status', (req,  res, next) => {
-    res.render('status', {
-      title: "Operation Status",
-      errors: {},
-      data: {
-      }
-    });
-    
+router.get('/operation-status', (req, res, next) => {
+  res.render('status', {
+    title: "Operation Status",
+    errors: {},
+    data: {}
+  });
+
 });
 
 module.exports = router;
-
