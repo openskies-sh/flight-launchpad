24114155088077e824159df74452efd4d4a9a3f4
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2021-03-20 23:19:48 +0000

    Added formatting and additional checks, logo

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# flight-launchpad
+
+<img src="https://i.imgur.com/zOB49qM.png" width="350">
+
 
 Flight Launchpad is a front end tool to submit the following data to Flight Blender and Spotlight:
 - Flight Declaration
 - Geofences
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const formidable = require('formidable');
-
+const { DateTime } = require("luxon");
 let geojsonhint = require("@mapbox/geojsonhint");
-let fs = require('fs');
 const {
   check,
-  validationResult,
-  matchedData
+  validationResult
 } = require('express-validator');
 
 
+const axios = require('axios');
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
@@ -41,8 +39,8 @@ router.post('/upload', flight_operation_validate, (req, res) => {
   //req.fields contains non-file fields 
   //req.files contains files 
   // res.send(JSON.stringify(req.fields));
-  // const form = formidable({ multiples: true });
-  console.log(req.body)
+  // const form = able({ multiples: true });
+  // console.log(req.body)
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
 
@@ -51,11 +49,50 @@ router.post('/upload', flight_operation_validate, (req, res) => {
       errors: errors.mapped()
     });
   }
-  const data = matchedData(req);
-  // console.log("Sanitized: ", data);
+  
+  
+  let date_range = req.body['datetimes'];
+  let date_split = date_range.split(' ');
+  let op_mode = req.body['operation_type'];
+  let op_name = req.body['operator_name'];
+  let geojson_upload = JSON.parse(req.body['geojson_upload_control']);
+  let start_date = DateTime.fromISO(date_split[0]);
+  let end_date = DateTime.fromISO(date_split[2]);
 
-  req.flash("success", "Thanks for the message! I‘ll be in touch :)");
-  res.redirect("/");
+  operation_mode_lookup = {'1':'vlos', '2':'bvlos'};
+
+  flight_declaration_json = {"start_time" :date_split[0], "end_time":date_split[1], "flight_declaration":{"exchange_type":"flight_declaration","originating_party": op_name, "flight_declaration":{"parts":geojson_upload}, "operation_mode":operation_mode_lookup[op_mode]}}
+
+
+  
+  
+  const base_url = process.env.BLENDER_BASE_URL || 'http://local.test:8000';
+  let url = base_url + '/set_flight_declaration'
+  axios.post(url, flight_declaration_json, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(function (blender_response) {
+
+    res.render('status', {
+      title: "Acceptance Status",
+      errors: {},
+      data: blender_response
+    });
+  })
+  .catch(function (error) {
+    console.log(error)
+
+    res.render('error-in-submission', {
+      title: "Error in submission",
+      errors: error,
+      data: {}
+    });
+  });
+
+  // req.flash("success", "Thanks for the message! I‘ll be in touch :)");
+  // res.redirect("/");
 
 });
 
